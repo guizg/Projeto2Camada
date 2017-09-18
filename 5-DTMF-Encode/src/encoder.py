724fc17033334af02aa2311f07f1025dac309876
guizg <guizgraicer@gmail.com>
2017-09-18 15:03:47 -0300

    fredao da massa

--- 5-DTMF-Encode/src/encoder.py
+++ 5-DTMF-Encode/src/encoder.py
@@ -1,15 +1,41 @@
 import sounddevice as sd
+import numpy as np
+import math
+import time
 
 fs = 44100
-duration = 2
+duration = 0.5
+freqsUpper = [1209, 1336, 1477, 1633]
+freqsLower = [697, 770, 852, 941]
 
-audio = sd.rec(int(duration*fs), fs, channels=1)
-sd.wait()
+# audio = sd.rec(int(duration*fs), fs, channels=1)
+# sd.wait()
 
-y = audio[:,0]
+# y = audio[:,0]
+
+x = np.linspace(0, duration, fs*duration)
+
+y9 = np.sin(2*math.pi*x*freqsUpper[2]) + np.sin(2*math.pi*x*freqsLower[2])
+y5 = np.sin(2*math.pi*x*freqsUpper[1]) + np.sin(2*math.pi*x*freqsLower[1])
+
+yh = np.sin(2*math.pi*x*512)
+ym = np.sin(2*math.pi*x*576)
 
 # reproduz o som
-sd.play(y, fs)
+sd.play(yh, fs)
+# aguarda fim da reprodução
+sd.wait()
 
+time.sleep(0.2)
+
+# reproduz o som
+sd.play(ym, fs)
+# aguarda fim da reprodução
+sd.wait()
+
+time.sleep(0.2)
+
+# reproduz o som
+sd.play(yh, fs)
 # aguarda fim da reprodução
 sd.wait()
