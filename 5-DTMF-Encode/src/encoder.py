6c5e14b3d168254e3c11fe25c95d8a1024c19493
vilela69 <frederico.vilelacurti@gmail.com>
2017-09-19 23:18:48 -0300

    fredao da massa 2

--- 5-DTMF-Encode/src/encoder.py
+++ 5-DTMF-Encode/src/encoder.py
@@ -2,40 +2,46 @@ import sounddevice as sd
 import numpy as np
 import math
 import time
+import matplotlib.pyplot as plt
 
+global fs,duration,x
 fs = 44100
 duration = 0.5
-freqsUpper = [1209, 1336, 1477, 1633]
-freqsLower = [697, 770, 852, 941]
+x = np.linspace(0, duration, fs * duration)
 
-# audio = sd.rec(int(duration*fs), fs, channels=1)
-# sd.wait()
+def createTonesArray():
+	freqsUpper = [1209, 1336, 1477, 1633]
+	freqsLower = [697, 770, 852, 941]
+	
+	tons = []
+	for i in range(4):
+		line = []
+		for j in range(4):
+			line.append(np.sin(2*math.pi*x*freqsUpper[j]) + np.sin(2*math.pi*x*freqsLower[i]))
+		tons.append(line)
+	return tons
 
-# y = audio[:,0]
+def playSound(freq):
+	sd.play(freq,fs)
+	plt.plot(x,freq)
+	sd.wait()
 
-x = np.linspace(0, duration, fs*duration)
+def humanMusic():
+    yh = np.sin(2*math.pi*x*512)
+    ym = np.sin(2*math.pi*x*576)
+    while True:
+        # reproduz o som
+        sd.play(yh, fs)
+        # aguarda fim da reprodução
+        sd.wait()
+        # reproduz o som
+        sd.play(ym, fs)
+        # aguarda fim da reprodução
+        sd.wait()
+        # reproduz o som
+        sd.play(yh, fs)
+        # aguarda fim da reprodução
+        sd.wait()
+        time.sleep(0.5)
 
-y9 = np.sin(2*math.pi*x*freqsUpper[2]) + np.sin(2*math.pi*x*freqsLower[2])
-y5 = np.sin(2*math.pi*x*freqsUpper[1]) + np.sin(2*math.pi*x*freqsLower[1])
 
-yh = np.sin(2*math.pi*x*512)
-ym = np.sin(2*math.pi*x*576)
-
-# reproduz o som
-sd.play(yh, fs)
-# aguarda fim da reprodução
-sd.wait()
-
-time.sleep(0.2)
-
-# reproduz o som
-sd.play(ym, fs)
-# aguarda fim da reprodução
-sd.wait()
-
-time.sleep(0.2)
-
-# reproduz o som
-sd.play(yh, fs)
-# aguarda fim da reprodução
-sd.wait()
---
+++ 5-DTMF-Encode/src/screen.py
@@ -0,0 +1,45 @@
+from tkinter import Tk, Button, RAISED
+from functools import partial
+import sounddevice as sd
+import numpy as np
+import math
+import time
+import encoder as encoder
+
+# audio = sd.rec(int(duration*fs), fs, channels=1)
+# sd.wait()
+# y = audio[:,0]
+class Screen:    
+    def __init__(self):
+        self.root = Tk()
+        self.fs = 44100
+        self.buttons = [
+            ['1','2','3','a'],
+            ['4','5','6','b'],
+            ['7','8','9','c'],
+            ['*','0','#','d']
+        ]
+        self.sounds = [512,576,640,682,768,854,960]
+        self.tons = encoder.createTonesArray()
+        
+        for r in range(4):
+            for c in range(4):
+                button = Button (
+                    self.root,
+                    relief = RAISED,
+                    padx = 10,
+                    text = self.buttons[r][c],
+                    command = partial(encoder.playSound, self.tons[r][c])
+                )
+                button.grid(row = r, column = c)
+                # freq = np.sin(2*math.pi*x*freqsUpper[2]) + np.sin(2*math.pi*x*freqsLower[2])
+
+
+                
+    def buttonClicked(self,buttonVal):
+        print(buttonVal)
+
+
+        
+Screen().root.mainloop()
+
