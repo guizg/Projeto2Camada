e69fc5066aa0520cb5b5c4d93d7a2752ab0eedce
guizg <guizgraicer@gmail.com>
2017-09-20 01:34:23 -0300

    fredao encoder da massa 5

--- 5-DTMF-Encode/src/decoder.py
+++ 5-DTMF-Encode/src/decoder.py
@@ -4,25 +4,28 @@ from numpy import linspace
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
+from threading import Thread
 class Servidor:
     def __init__(self):
         self.app = Flask(__name__)
+        self.app.before_request(lambda: Thread(target=self.listen).start())
         self.app.add_url_rule('/listen','listen',self.onRequest)
         self.fs = 44100
         self.listenDuration = 1
         self.x = linspace(0, self.listenDuration, self.fs * self.listenDuration)
-        self.app.before_request(self.listen)
-        self.app.run()
+        
+        self.app.run(host="0.0.0.0")
 
     def onRequest(self):
-        return "OK",
+        return "OK"
 
     def listen(self):
         print('Listening...')
         audio = sd.rec(int(self.listenDuration * self.fs),self.fs,channels=1)
         sd.wait()
         y = audio[:,0]
-        plt.plot(self.x,y)
+        plt.clf()
+        plt.plot(self.x[40000:40200],y[40000:40200])
         print('Finished listening...')
         plt.show()
 
